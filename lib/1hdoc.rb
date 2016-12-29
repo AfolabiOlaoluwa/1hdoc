a737f83dd26339ab0497b6b490448a0bd850b530
Dom <dom.corvasce@yandex.com>
2016-12-29 17:48:53 +0100

    Add lib/1hdoc.rb

---
+++ lib/1hdoc.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+# stdlib
+require 'yaml'
+
+# 1hdoc core
+require_relative '1hdoc/utilities'
+require_relative '1hdoc/log_builder'
+require_relative '1hdoc/log_viewer'
