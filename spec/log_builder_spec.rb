c52024d03b577a84fcf2a9ce0df7f6cce6989079
Dom <dom.corvasce@yandex.com>
2016-12-29 17:45:00 +0100

    Refactor unit tests

--- spec/log_builder_spec.rb
+++ spec/log_builder_spec.rb
@@ -8,37 +8,43 @@ describe HDOC::LogBuilder do
     subject.add(day: 2, progress: 'Fixed CSS', thoughts: '--', links: {})
   end
 
-  it 'should add a new record to the log' do
-    expect(subject.log[2]).not_to be_nil
-  end
-
-  it 'should edit a record' do
-    subject.edit(2, progress: 'Fixed CSS.')
-    expect(subject.log[2]['progress']).to eq('Fixed CSS.')
-  end
-
-  it 'should write the edits on file' do
-    filename = expand_path('fixtures/log_example_2.yaml')
-    log = described_class.new(filename)
-
-    log.add(day: 2, progress: 'Fixed CSS', thoughts: '--', links: {})
-    log.build
-
-    expect(File.read(filename)).to include('Fixed CSS')
-    File.open(filename, 'w') { |file| file.puts '---' }
-  end
-
-  it 'should transform symbols keys to string ones' do
-    expect(subject.log[2]['progress']).to eq('Fixed CSS')
-  end
-
   it 'should raise an error if log file is not found' do
     expected_error = HDOC::Utilities::LogNotFound
     expect { described_class.new('u2/log.yaml') }.to raise_error(expected_error)
   end
 
-  it 'should raise an error if day is not found' do
-    expected_error = HDOC::Utilities::DayNotFound
-    expect { subject.edit(21, progress: 'None') }.to raise_error(expected_error)
+  context '#add' do
+    it 'should add a new record to the log' do
+      expect(subject.log[2]).not_to be_nil
+    end
+
+    it 'should transform symbols keys to string ones' do
+      expect(subject.log[2]['progress']).to eq('Fixed CSS')
+    end
+  end
+
+  context '#edit' do
+    it 'should edit a record' do
+      subject.edit(2, progress: 'Fixed CSS.')
+      expect(subject.log[2]['progress']).to eq('Fixed CSS.')
+    end
+
+    it 'should raise an error if day is not found' do
+      expected_error = HDOC::Utilities::DayNotFound
+      expect { subject.edit(21, progress: 'None') }.to raise_error(expected_error)
+    end
+  end
+
+  context '#build' do
+    it 'should write the edits on file' do
+      filename = expand_path('fixtures/log_example_2.yaml')
+      log = described_class.new(filename)
+
+      log.add(day: 2, progress: 'Fixed CSS', thoughts: '--', links: {})
+      log.build
+
+      expect(File.read(filename)).to include('Fixed CSS')
+      File.open(filename, 'w') { |file| file.puts '---' }
+    end
   end
 end
--- spec/log_viewer_spec.rb
+++ spec/log_viewer_spec.rb
@@ -4,13 +4,19 @@ require_relative '../lib/1hdoc/log_viewer'
 describe HDOC::LogViewer do
   subject { described_class.new('./fixtures/log_example.yaml') }
 
-  it 'should show the whole log' do
-    expect(subject.show).to include('- Day 1 -')
-    expect(subject.show).to include('- Day 2 -')
-  end
+  context '#show' do
+    it 'should return the whole log' do
+      expect(subject.show).to include('- Day 1 -')
+      expect(subject.show).to include('- Day 2 -')
+    end
 
-  it 'should show a specific record' do
-    expect(subject.show(2)).not_to include('- Day 1 -')
-    expect(subject.show(2)).to include('- Day 2 -')
+    it 'should return a specific record' do
+      expect(subject.show(2)).not_to include('- Day 1 -')
+      expect(subject.show(2)).to include('- Day 2 -')
+    end
+
+    it 'should return anything if the given day is invalid' do
+      expect(subject.show(-2)).to eq('')
+    end
   end
 end
