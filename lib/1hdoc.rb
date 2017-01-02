f5d3e205de5dab9ee609d98d570874e50d877e21
Dom <dom.corvasce@yandex.com>
2017-01-02 15:01:15 +0100

    Update lib/1hdoc.rb and bin/1hdoc

--- lib/1hdoc.rb
+++ lib/1hdoc.rb
@@ -1,8 +1,11 @@
 # stdlib
 require 'yaml'
+require 'git'
 
 # 1hdoc core
 require_relative '1hdoc/utilities'
 require_relative '1hdoc/configuration'
 require_relative '1hdoc/log_builder'
 require_relative '1hdoc/log_viewer'
+
+
